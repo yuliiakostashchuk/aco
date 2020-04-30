f01a0ae9142a00158a6f5bb5e60037b22a107a69
Julia Kostashchuk <ykostash@redhat.com>
2020-04-30 16:28:14 +0200

    Read TSP file

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 def main():
-    pass
+    with open('eil51.tsp') as f:
+        for line in f:
+            if line.startswith('DIMENSION :'):
+                print(line.split()[-1])
+            if line.startswith('NODE_COORD_SECTION'):
+                print('found section containing the node coordinates')
+                break
 
 if __name__ == '__main__':
     main()
