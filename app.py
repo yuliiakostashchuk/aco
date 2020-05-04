c3c47f7de91bc970156c49ff5e8c7dc553f3dfb4
Julia Kostashchuk <ykostash@redhat.com>
2020-05-04 14:43:30 +0200

    Set default parameters

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 import datetime
 
+def set_default_parameters():
+    parameters = {
+        "ls_flag": 3,
+        "dlb_flag": True,
+        "nn_ls": 20,
+        "n_ants": 25,
+        "nn_ants": 20,
+        "alpha": 1.0,
+        "beta": 2.0,
+        "rho": 0.5,
+        "q_0": 0.0,
+        "max_tries": 10,
+        "max_tours": 0,
+        "seed": None, # TODO
+        "max_time": 10.0,
+        "optimal": 1,
+        "branch_fac": 1.00001,
+        "u_gb": None, # TODO
+        "as_flag": False,
+        "eas_flag": False,
+        # "ras_flag": False,
+        "mmas_flag": False,
+        # "bwas_flag": False,
+        "acs_flag": False,
+        "ras_ranks": 0,
+        "elitist_ants": 0
+    }
+    return parameters
+
 def main():
     start_time = datetime.datetime.now()
     with open('eil51.tsp') as f:
