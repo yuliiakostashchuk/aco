b3cfe88e12a925e488af1f100c7cd0cd56333072
Julia Kostashchuk <ykostash@redhat.com>
2020-05-04 10:33:29 +0200

    Measure elapsed time

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import datetime
+
 def main():
+    start_time = datetime.datetime.now()
     with open('eil51.tsp') as f:
         for line in f:
             if line.startswith('DIMENSION :'):
@@ -6,6 +9,8 @@ def main():
             if line.startswith('NODE_COORD_SECTION'):
                 print('found section containing the node coordinates')
                 break
+    elapsed = datetime.datetime.now() - start_time
+    print(elapsed.seconds, elapsed.microseconds)
 
 if __name__ == '__main__':
     main()
