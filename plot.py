ebc82e6496e0f084a09d21872ac86e3e9be0d21d
Julia Kostashchuk <ykostash@redhat.com>
2020-05-16 11:07:39 +0200

    Add main

--- plot.py
+++ plot.py
@@ -6,3 +6,6 @@ def get_circumference(radius, n):
     for i in range(n):
         points.append((int(radius*math.cos(2*pi/n*i)), int(radius*math.sin(2*pi/n*i))))
     return points
+
+if __name__ == '__main__':
+    pass
