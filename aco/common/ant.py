45d4999f85517496766033e897f70c5de4fa394e
Julia Kostashchuk <ykostash@redhat.com>
2020-06-19 11:55:41 +0200

    Remove tour_length @property

--- aco/common/ant.py
+++ aco/common/ant.py
@@ -23,18 +23,6 @@ class Ant:
         self.tour.append(node)
         self.visited.add(node)
 
-    @property
-    def tour_length(self):
-
-        tour_length = 0
-        distances = self.instance.distances
-
-        for i in range(self.n):
-            distance = distances[self.tour[i]][self.tour[i + 1]]
-            tour_length += distance
-
-        return tour_length
-
     @classmethod
     def nn_tour(cls, instance):
 
